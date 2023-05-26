9d8310b3f9a4e71271dcc73bb4c6cf0b2e3830b6
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2023-05-25 20:45:13 -0600

    Main Form

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace List_Manipulator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
