cc2717d8a17544b36f33a39b07689d3c3edcb9bc
Michelle Alzola <105762453+michellealzola@users.noreply.github.com>
2023-05-27 06:51:54 -0600

    Added Controls

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,20 +28,100 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lBoxOrigList = new System.Windows.Forms.ListBox();
+            this.lBoxResults = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // Form1
+            // lBoxOrigList
+            // 
+            this.lBoxOrigList.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lBoxOrigList.FormattingEnabled = true;
+            this.lBoxOrigList.ItemHeight = 26;
+            this.lBoxOrigList.Location = new System.Drawing.Point(21, 48);
+            this.lBoxOrigList.Name = "lBoxOrigList";
+            this.lBoxOrigList.Size = new System.Drawing.Size(348, 238);
+            this.lBoxOrigList.TabIndex = 0;
+            // 
+            // lBoxResults
+            // 
+            this.lBoxResults.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lBoxResults.FormattingEnabled = true;
+            this.lBoxResults.ItemHeight = 26;
+            this.lBoxResults.Location = new System.Drawing.Point(395, 48);
+            this.lBoxResults.Name = "lBoxResults";
+            this.lBoxResults.Size = new System.Drawing.Size(348, 238);
+            this.lBoxResults.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(21, 345);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(348, 58);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Remove Negative Numbers";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(395, 345);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(348, 58);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Find Values 1 to 10";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 26);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Original List";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arsenal", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(390, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 26);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Results";
+            // 
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(769, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lBoxResults);
+            this.Controls.Add(this.lBoxOrigList);
+            this.Name = "MainForm";
             this.Text = "List Manipulator";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lBoxOrigList;
+        private System.Windows.Forms.ListBox lBoxResults;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
